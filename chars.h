e0c7a529587038339b0a57387e6fcaedf207cc75
michael terranova <michael terranova>
2016-12-17 00:36:12 -0500

    Added the chars.h as a starting point(wewlad this is gonna be long)

---
+++ chars.h
@@ -0,0 +1,9 @@
+//charcater headerfile that will be storing all information (structs and such) regarding charcater creation
+
+
+//compare integers for us to use as stats
+int comp(const void* num1, const void* num2);
+
+//roll a random integer between 1 and the specifed die
+int roll(int dnum);
+
